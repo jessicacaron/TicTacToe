c77868d09f043537e02ffe0c882ee569dd7cad96
jessicacaron <77312057+jessicacaron@users.noreply.github.com>
2023-08-16 17:29:40 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// TicTacToe class to control the game logic
+        /// </summary>
+        clsTicTacToe TicTacToe;
+        /// <summary>
+        /// Checks to see if the game is active or not started
+        /// </summary>
+        bool started;
+        /// <summary>
+        /// Keeps track of who's turn it is during the game
+        /// </summary>
+        bool player1;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //create instance of TicTacToe
+            TicTacToe = new clsTicTacToe();
+        }
+
+        #region Click Methods
+
+        /// <summary>
+        /// Method starts when start button is clicked.  It loads initial game data.
+        /// </summary>
+        /// <param name="sender">Start button click object info</param>
+        /// <param name="e">event data</param>
+        private void btnStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            //set up initial game board, player 1 to start, start game
+            player1 = true;
+            txtStatus.Text = "Player 1's turn";
+            for (int i = 0; i < 9; i++)
+            {
+                TicTacToe.SetsaBoard(i, "");
+            }
+            started = true;
+            player1 = true;
+
+            //reset colors 
+            resetColors();
+
+            //reselt labels(buttons) 
+            resetLabels();
+        }
+
+        /// <summary>
+        /// This method activates when player clicks on a cell to make a move, then calls on the TicTacToe class to check 
+        /// for and calculate a win, and then highlights the necessary cells in the game to display winning move.
+        /// </summary>
+        /// <param name="sender">Any click from the board </param>
+        /// <param name="e"></param>
+        private void PlayerMoveClick(object sender, RoutedEventArgs e)
+        {
+            //make sure game is active
+            if (started)
+            {
+                //make sure space isnt already clicked
+                var button = (Button)sender;
+                if (button.Content.ToString() == " ")
+                {
+                    //load in players move
+                    string move = button.Name;
+                    int position = Int32.Parse(move.Last().ToString());
+
+                    //load board method
+                    button.Content = player1 ? "X" : "O";
+                    loadBoard(position);
+
+                    //check for tie
+                    int check = 0;
+                    for (int i = 0; i < 9; i++)
+                    {
+                        if (TicTacToe.GetsaBoard()[i] != "")
+                        {
+                            check++;
+                        }
+                    }
+
+                    //check to see if the last move is a winning move
+                    if (TicTacToe.IsWinningMove())
+                    {
+                        //if winning move, highlight col/row/diag on the board
+                        string toHighlight = TicTacToe.GeteWinningMove();
+                        if (toHighlight == "row1")
+                        {
+                            btnBoard0.Background = Brushes.Aqua;
+                            btnBoard1.Background = Brushes.Aqua;
+                            btnBoard2.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "row2")
+                        {
+                            btnBoard3.Background = Brushes.Aqua;
+                            btnBoard4.Background = Brushes.Aqua;
+                            btnBoard5.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats() ;
+                        }
+                        else if (toHighlight == "row3")
+                        {
+                            btnBoard6.Background = Brushes.Aqua;
+                            btnBoard7.Background = Brushes.Aqua;
+                            btnBoard8.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "col1")
+                        {
+                            btnBoard0.Background = Brushes.Aqua;
+                            btnBoard3.Background = Brushes.Aqua;
+                            btnBoard6.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "col2")
+                        {
+                            btnBoard1.Background = Brushes.Aqua;
+                            btnBoard4.Background = Brushes.Aqua;
+                            btnBoard7.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "col2")
+                        {
+                            btnBoard2.Background = Brushes.Aqua;
+                            btnBoard5.Background = Brushes.Aqua;
+                            btnBoard8.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "diag1")
+                        {
+                            btnBoard0.Background = Brushes.Aqua;
+                            btnBoard4.Background = Brushes.Aqua;
+                            btnBoard8.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                        else if (toHighlight == "diag2")
+                        {
+                            btnBoard2.Background = Brushes.Aqua;
+                            btnBoard4.Background = Brushes.Aqua;
+                            btnBoard6.Background = Brushes.Aqua;
+                            //update stats
+                            updateStats();
+                        }
+                    }
+                    
+                    //highlight whole board in the event of a tie
+                    else if (check == 9)
+                    {
+                        btnBoard0.Background = Brushes.Aqua;
+                        btnBoard1.Background = Brushes.Aqua;
+                        btnBoard2.Background = Brushes.Aqua;
+                        btnBoard3.Background = Brushes.Aqua;
+                        btnBoard4.Background = Brushes.Aqua;
+                        btnBoard5.Background = Brushes.Aqua;
+                        btnBoard6.Background = Brushes.Aqua;
+                        btnBoard7.Background = Brushes.Aqua;
+                        btnBoard8.Background = Brushes.Aqua;
+                        TicTacToe.SetiTies();
+                        lblTies.Content = "Ties: " + TicTacToe.GetiTies();
+                        txtStatus.Text = "The game is a tie";
+
+                    }
+
+                    //if there is no tie and no winning move, set up next player turn and update status board
+                    else
+                    {
+                        //set player to next
+                        if (player1)
+                        {
+                            txtStatus.Text = "Player 2's turn";
+                            player1 = false;
+                        }
+                        else if (!player1)
+                        {
+                            txtStatus.Text = "Player 1's turn";
+                            player1 = true;
+                        }
+                    } 
+
+                }
+
+            }
+
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Method to update TicTacToe variable data(wins/ties) so correct information is displayed.
+        /// </summary>
+        private void updateStats()
+        {
+            if (player1)
+            {
+                txtStatus.Text = "Player 1 Wins";
+                TicTacToe.SetPlayer1Wins();
+                lblPlayer1Wins.Content = "Player 1 Wins: " + TicTacToe.GetiPlayer1Wins();
+            }
+            else if (!player1)
+            {
+                TicTacToe.SetPlayer2Wins();
+                lblPlayer2Wins.Content = "Player 2 Wins: " + TicTacToe.GetiPlayer2Wins();
+                txtStatus.Text = "Player 2 Wins";
+            }
+            started = false;
+        }
+
+        /// <summary>
+        /// Method to reset cell colors back to the original start state.
+        /// </summary>
+        private void resetColors()
+        {
+            btnBoard0.Background = Brushes.LightGray;
+            btnBoard1.Background = Brushes.LightGray;
+            btnBoard2.Background = Brushes.LightGray;
+            btnBoard3.Background = Brushes.LightGray;
+            btnBoard4.Background = Brushes.LightGray;
+            btnBoard5.Background = Brushes.LightGray; 
+            btnBoard6.Background = Brushes.LightGray;
+            btnBoard7.Background = Brushes.LightGray;
+            btnBoard8.Background = Brushes.LightGray;
+        }
+
+        /// <summary>
+        /// Method sets all labels back to the original start state.
+        /// </summary>
+        private void resetLabels()
+        {
+            btnBoard0.Content = " ";
+            btnBoard1.Content = " ";
+            btnBoard2.Content = " ";
+            btnBoard3.Content = " ";
+            btnBoard4.Content = " ";
+            btnBoard5.Content = " ";
+            btnBoard6.Content = " ";
+            btnBoard7.Content = " ";
+            btnBoard8.Content = " ";   
+        }
+
+        /// <summary>
+        /// Access property for board and sets string saboard based on label position.
+        /// </summary>
+        /// <param name="position">Position of the cell that was played on the board.</param>
+        private void loadBoard(int position)
+        {
+            if (player1)
+            {
+                TicTacToe.SetsaBoard(position, "X");
+            }
+            else 
+            {
+                TicTacToe.SetsaBoard(position, "0");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ clsTicTacToe.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// TicTacToe class creates an instance of game logic for a tictactoe game to be played.
+    /// </summary>
+    public class clsTicTacToe
+    {
+        /// <summary>
+        /// Array that holds current content of each move ("X" or "O")
+        /// </summary>
+        private string[] saBoard;
+        /// <summary>
+        /// Holds the total number of Player 1 wins
+        /// </summary>
+        private int iPlayer1Wins;
+        /// <summary>
+        /// Holds the total number of Player 2 wins
+        /// </summary>
+        private int iPlayer2Wins;
+        /// <summary>
+        /// Holds the total number of ties
+        /// </summary>
+        private int iTies;
+        /// <summary>
+        /// Holds the cell position of the winning move using descriptive string, ie. row1
+        /// </summary>
+        private string eWinningMove;
+
+
+        /// <summary>
+        /// Constructor for TicTacToe class sets the board to 0 and creates empty board
+        /// </summary>
+        public clsTicTacToe()
+        {
+            //initialize saBoard with new
+            saBoard = new string[9];
+            iPlayer1Wins = 0;
+            iPlayer2Wins = 0;
+            iTies = 0;
+            eWinningMove = "";
+        }
+
+        #region Getter Methods
+
+        /// <summary>
+        /// Returns the current state of the game board and its cell contents in an array
+        /// </summary>
+        /// <returns>String array of values X or O, or " " if not yet clicked</returns>
+        public string[] GetsaBoard()
+        {
+            return saBoard;
+        }
+        /// <summary>
+        /// Int that stores current player 1 wins is returned
+        /// </summary>
+        /// <returns>Returns the current player 1 wins</returns>
+        public int GetiPlayer1Wins()
+        {
+            return iPlayer1Wins;
+        }
+        /// <summary>
+        /// Int that stores current player 2 wins is returned
+        /// </summary>
+        /// <returns>Returns the current player 2 wins</returns>
+        public int GetiPlayer2Wins()
+        {
+            return iPlayer2Wins;
+        }
+        /// <summary>
+        /// Int that stores current number of ties is returned
+        /// </summary>
+        /// <returns>Returns the current number of ties</returns>
+        public int GetiTies()
+        {
+            return iTies;
+        }
+        /// <summary>
+        /// String that stores the winning move cells is returned
+        /// </summary>
+        /// <returns>Returns the current winning move</returns>
+        public string GeteWinningMove()
+        {
+            return eWinningMove;
+        }
+
+        #endregion
+
+
+        #region Setter Methods
+
+        /// <summary>
+        /// Sets the current game board at position the value passed
+        /// </summary>
+        public void SetsaBoard(int index, string value)
+        {
+            saBoard[index] = value;
+        }
+        /// <summary>
+        /// Sets current player 1 wins
+        /// </summary>
+        public void SetPlayer1Wins()
+        {
+            iPlayer1Wins++;
+        }
+        /// <summary>
+        /// Sets current player 2 wins
+        /// </summary>
+        public void SetPlayer2Wins()
+        {
+            iPlayer2Wins++;
+        }
+        /// <summary>
+        /// Sets current ties
+        /// </summary>
+        public void SetiTies()
+        {
+            iTies++;
+        }
+
+        #endregion
+
+
+        #region Other Methods
+
+        /// <summary>
+        /// Method calculates if the move made was a winning move or not.
+        /// </summary>
+        /// <returns>If winning move, method returns the cells involved in the win.</returns>
+        public bool IsWinningMove()
+        {
+            //check for winning move
+            //row1
+            if (saBoard[0] !="" && saBoard[0] == saBoard[1] && saBoard[0] == saBoard[2])
+            {
+                eWinningMove = "row1";
+                return true;
+            }
+
+            //col1
+            else if (saBoard[0] != "" && saBoard[0] == saBoard[3] && saBoard[0] == saBoard[6])
+            {
+                eWinningMove = "col1";
+                return true;
+            }
+
+            //row 2
+            else if (saBoard[3] != "" && saBoard[3] == saBoard[4] && saBoard[3] == saBoard[5])
+            {
+                eWinningMove = "row2";
+                return true;
+            }
+
+            //col2
+            else if (saBoard[1] != "" && saBoard[1] == saBoard[4] && saBoard[1] == saBoard[7])
+            {
+                eWinningMove = "col2";
+                return true;
+            }
+
+            //row3
+            else if (saBoard[6] != "" && saBoard[6] == saBoard[7] && saBoard[6] == saBoard[8])
+            {
+                eWinningMove = "row3";
+                return true;
+            }
+
+            //col3
+            else if (saBoard[2] != "" && saBoard[2] == saBoard[5] && saBoard[2] == saBoard[8])
+            {
+                eWinningMove = "col3";
+                return true;
+            }
+
+            //diag1
+            else if (saBoard[0] != "" && saBoard[0] == saBoard[4] && saBoard[0] == saBoard[8])
+            {
+                eWinningMove = "diag1";
+                return true;
+            }
+
+            //diag2
+            else if (saBoard[2] != "" && saBoard[2] == saBoard[4] && saBoard[2] == saBoard[6])
+            {
+                eWinningMove = "diag2";
+                return true;
+            }
+
+            //return false if it is not a winning move
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
